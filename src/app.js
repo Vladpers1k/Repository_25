065ec97f3de3718312bd6c989c4d528f455b7ba7
Vladpers1k <vladik340123@gmail.com>
2025-04-01 14:31:19 +0300

    big changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('./db')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors()) // Додано
 app.use(express.json())
 app.use('/api', routes)
 
